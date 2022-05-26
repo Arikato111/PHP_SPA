675739c5a3ba5942dc96c1cbcad31fd32a14121e
Nawasan <arikato110011@gmail.com>
2022-05-26 09:26:57 +0700

    change title in home.php

--- page/home.php
+++ page/home.php
@@ -1,10 +1,10 @@
 <?php
 function HomePage(){
-    return '<a href="about">about</a>
-    <hr>
-    <h2>this is home page</h2>
-    <div>Content</div>
-    <div>click "about" to go to about page</div>'
-    . '<script>document.title = "home"</script>'; // use javascript to change title
-    
+    return title("Home")
+            .'<a href="about">about</a>
+            <hr>
+            <h2>this is home page</h2>
+            <div>Content</div>
+            <div>click "about" to go to about page</div>'
+        ;    
 }
