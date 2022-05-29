86a8f374293c817b5bc5934a3a51da1e6d1204c6
Nawasan <arikato110011@gmail.com>
2022-05-29 22:06:25 +0700

    Release version

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
-require("./path.php");
+// Don't Delete or Change
+require('./lib/wisit-single-page.php');
+require("./package.php");
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,15 +12,15 @@ require("./path.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>title</title>
-    <link rel="stylesheet" href="/style.css"> 
-    <!-- do not use ./style.css or style.css -->
+    <link rel="stylesheet" href="/static/style.css"> 
+    <!-- do not use ./style.css or style.css, only use /style.css -->
 </head>
 
 <body>
     <div>
         <?php echo RootContent(); ?>
     </div>
-    <script src="/script.js"></script>
+    <script src="/static/script.js"></script>
 </body>
 
 </html>
--- lib/wisit-single-page.php
+++ lib/wisit-single-page.php
@@ -1,5 +1,10 @@
 <?php
-// func for check path to use switch case
+/*  * Copyright (c) 2022 ณวสันต์ วิศิษฏ์ศิงขร
+    *
+    * This source code is licensed under the MIT license found in the
+    * LICENSE file in the root directory of this source tree.
+*/
+
 function getPath(){
     $link = "$_SERVER[REQUEST_URI]";
     $real_path = "/";
