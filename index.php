a721299e68a3284da535865154cbb0bb4736dc8d
Nawasan <arikato110011@gmail.com>
2022-05-31 20:52:11 +0700

    commit file installer

--- index.php
+++ index.php
@@ -1,26 +1,48 @@
 <?php
-session_start();
-// Don't Delete or Change
-require('./modules/wisit-single-page.php');
-require("./package.php");
-?>
-<!DOCTYPE html>
-<html lang="en">
+mkdir('modules');
+mkdir('src');
+mkdir('static');
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>title</title>
-    <link rel="stylesheet" href="/static/style.css"> 
-    <!-- do not use ./style.css or style.css, only use /style.css -->
-</head>
+ $package = [
+     [
+         "file_name"=>"modules/LICENSE",
+         "file_link"=>"https://raw.githubusercontent.com/Arikato111/PHP_SPA/Release/modules/LICENSE",
+     ],
+     [
+         "file_name"=>"modules/wisit-single-page.php",
+         "file_link"=>"https://raw.githubusercontent.com/Arikato111/PHP_SPA/Release/modules/wisit-single-page.php",
+     ],
+     [
+         "file_name"=>"src/home.php",
+         "file_link"=>"https://raw.githubusercontent.com/Arikato111/PHP_SPA/Release/src/home.php",
+     ],
+     [
+         "file_name"=>"src/main.php",
+         "file_link"=>"https://raw.githubusercontent.com/Arikato111/PHP_SPA/Release/src/main.php",
+     ],
+     [
+         "file_name"=>"static/script.js",
+         "file_link"=>"https://raw.githubusercontent.com/Arikato111/PHP_SPA/Release/static/script.js",
+     ],
+     [
+         "file_name"=>"src/style.css",
+         "file_link"=>"https://raw.githubusercontent.com/Arikato111/PHP_SPA/Release/static/style.css",
+     ],
+     [
+         "file_name"=>".htaccess",
+         "file_link"=>"https://raw.githubusercontent.com/Arikato111/PHP_SPA/Release/.htaccess",
+     ],
+     [
+         "file_name"=>"package.php",
+         "file_link"=>"https://raw.githubusercontent.com/Arikato111/PHP_SPA/Release/package.php",
+     ],
+     [
+         "file_name"=>"index.php",
+         "file_link"=>"https://raw.githubusercontent.com/Arikato111/PHP_SPA/Release/index.php",
+     ]
+];
 
-<body>
-    <div>
-        <?php echo RootContent(); ?>
-    </div>
-    <script src="/static/script.js"></script>
-</body>
-
-</html>
+foreach($package as $file){
+    $module  = file_get_contents($file['file_link']);
+    file_put_contents($file['file_name'], $module);
+}
