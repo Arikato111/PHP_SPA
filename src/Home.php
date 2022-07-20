0e75a3df33d2d94db3df10764740878fd6082e20
nawasan <arikato110011@gmail.com>
2022-07-20 08:47:21 +0700

    change display

--- src/Home.php
+++ src/Home.php
@@ -9,31 +9,16 @@ $Home = function () use ($title) {
       <link rel="stylesheet" href="/public/home.css">
 
       <!-- Content  -->
-      <div class="box">
-        <div class="title">Welcome to PHP_SPA version 3.0 </div>
-        <hr>
-        <div class="subtitle">Writing code with </div>
-        <div class="box-list">
-          <ul class="list">
-            <li>SwitchPath</li>
-            <li>Route </li>
-            <li>getParams</li>
-            <li>getPath </li>
-            <li>title</li>
-            <li>import</li>
-          </ul>
+      <div>
+        <div class="box">
+          <div class="triangle"></div>
+          <div class="triangle fixed"></div>
         </div>
-        <div class="subtitle">And most importantly, use 
-          <a href="https://github.com/Arikato111/PHP_SPA/tree/Release3.0#import"
-          style="text-decoration: none;color:black;"
-          target="_blank"
-          >
-            <b>import</b>
-          </a>
-          to require file or module</div>
-        <div> writing page as function</div>
-        <div style="font-size: 18px;">You will find style.css and script.js in static folder.</div>
-        <h4 class="contact">Read more at <a class="hove" target="_blank" href="https://github.com/Arikato111/PHP_SPA">Github</a></h4>
+        <h1 align="center">Welcome to PHP_SPA 3.0</h1>
+        <h2 align="center">
+          Read more at 
+          <a href="https://github.com/Arikato111/PHP_SPA">Github</a>
+        </h2>
       </div>
     </div>
     HTML;
