97fa4d6db46deae708d1ffac9d714ec2c1c33daf
nawasan <arikato110011@gmail.com>
2022-07-25 14:47:00 +0700

    update little Main

--- src/Main.php
+++ src/Main.php
@@ -1,8 +1,8 @@
 <?php
 session_start(); // <-- you can delete if don't need to use
-
 ['SwitchPath' => $SwitchPath, 'Route' => $Route] = import('wisit-router');
 $HomePage = import('./src/Home');
+
 $Main = function () use ($SwitchPath, $Route, $HomePage) {
     return $SwitchPath(
         $Route('/', fn () => $HomePage()), // using callback to return Page function
