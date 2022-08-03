2dae6ae40b7f6bbdf940fa55994336542484424f
Nawasan <arikato110011@gmail.com>
2022-08-03 21:02:22 +0700

    add icon

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $Content = $Main(); ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/public/icon.svg" type="image/svg">
     <title><?php echo $GLOBALS['title']; ?></title>
     <link rel="stylesheet" href="/public/style.css">
     <?php $showStyle(); ?>
